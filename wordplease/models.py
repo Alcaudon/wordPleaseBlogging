280cc7a2abe5a85963f706e25eb0df820a29ed8b
Luis Barriga <barbaclu@gmail.com>
2018-01-01 23:06:41 +0100

    Generamos el modelo y la BBDD

--- wordplease/models.py
+++ wordplease/models.py
@@ -1,3 +1,25 @@
 from django.db import models
 
-# Create your models here.
+
+class Category(models.Model):
+    name = models.CharField(max_length=50)
+    description = models.TextField(blank=True, null=True)
+
+    def __str__(self):
+        return self.name
+
+
+class Post(models.Model):
+    title = models.CharField(max_length=150)
+    introduction = models.CharField(max_length=300)
+    body = models.TextField()
+    URL = models.URLField(blank=True, null=True)
+    date = models.DateTimeField()
+
+    created_at = models.DateTimeField(auto_now_add=True)
+    modified_at = models.DateTimeField(auto_now=True)
+
+    category = models.ForeignKey(Category, on_delete=models.PROTECT)
+
+    def __str__(self):
+        return self.title
