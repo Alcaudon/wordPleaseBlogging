7ad8363031c0567fa85e6e23d31d897b45973b36
Luis Barriga <barbaclu@gmail.com>
2018-01-02 20:52:52 +0100

    Implementamos la navegación del listado de posts al detalle. Mejoramos los querys a la BBDD y el harcodeo de rutas en las plantillas

--- wordPleaseBlogging/urls.py
+++ wordPleaseBlogging/urls.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
 from django.urls import path
-from wordplease.views import home
+from wordplease.views import home, post_detail
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', home),
+    path('posts/<int:pk>', post_detail, name="post_detail_name")
 ]
--- wordplease/views.py
+++ wordplease/views.py
@@ -6,3 +6,11 @@ def home(request):
     context = {'posts': posts}
     return render(request, "home.html", context)
 
+def post_detail(request, pk):
+    possible_posts = Post.objects.filter(pk=pk).select_related('category')
+    if len(possible_posts) == 0:
+        return render(request, "404.html", status=404)
+    else:
+        post = possible_posts[0]
+        context = {'post': post}
+        return render(request, "post_detail.html", context)
