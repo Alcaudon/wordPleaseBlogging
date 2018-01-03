93b954b9ce54d28ee2809dd5bcb0e9bafede3ad1
Luis Barriga <barbaclu@gmail.com>
2018-01-03 22:40:06 +0100

    Creamos el menú de navegación en función de si el usuario está autenticado

--- wordPleaseBlogging/urls.py
+++ wordPleaseBlogging/urls.py
@@ -1,12 +1,13 @@
 from django.contrib import admin
 from django.urls import path
 from wordplease.views import home, post_detail
-from users.views import login
+from users.views import login, logout
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', home, name="home_page"),
     path('posts/<int:pk>', post_detail, name="post_detail_name"),
-    path('login', login, name='login_page')
+    path('login', login, name='login_page'),
+    path('logout', logout, name='logout_page')
 ]
