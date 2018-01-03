200b00fd73c68481ba655ba52f3ff341119bd715
Luis Barriga <barbaclu@gmail.com>
2018-01-03 21:37:48 +0100

    Implementamos el login de usuario con Django

--- wordPleaseBlogging/settings.py
+++ wordPleaseBlogging/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'wordplease.apps.WordpleaseConfig',
+    'users.apps.UsersConfig',
 ]
 
 MIDDLEWARE = [
--- wordPleaseBlogging/urls.py
+++ wordPleaseBlogging/urls.py
@@ -1,9 +1,12 @@
 from django.contrib import admin
 from django.urls import path
 from wordplease.views import home, post_detail
+from users.views import login
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', home, name="home_page"),
-    path('posts/<int:pk>', post_detail, name="post_detail_name")
+    path('posts/<int:pk>', post_detail, name="post_detail_name"),
+    path('login', login, name='login_page')
 ]
