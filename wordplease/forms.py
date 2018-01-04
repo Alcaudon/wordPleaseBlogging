95f08ca8cceee8fe87ef5698c7c48d094dd8332c
Luis Barriga <barbaclu@gmail.com>
2018-01-04 23:03:38 +0100

    Creamos el formulario de creación de una película

---
+++ wordplease/forms.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+from django import forms
+from django.forms import ModelForm
+
+from wordplease.models import Post
+
+
+class PostForm(ModelForm):
+    class Meta:
+        model = Post
+        fields = '__all__'
