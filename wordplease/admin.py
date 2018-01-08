ae9747097d9782ff09a0d10186e61963bfbb7d7f
Luis Barriga <barbaclu@gmail.com>
2018-01-08 19:52:24 +0100

    Realizamos modificaciones al admin de Django para personificar la vista del Post

--- wordplease/admin.py
+++ wordplease/admin.py
@@ -3,4 +3,14 @@ from django.contrib import admin
 from wordplease.models import Category, Post
 
 admin.site.register(Category)
-admin.site.register(Post)
+
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    list_display = ('title', 'introduction', 'date', 'user_full_name', 'category')
+    list_filter = ('user', 'category')
+    search_fields = ('title', 'introduction')
+
+    def user_full_name(self, obj):
+        return "{0} {1}".format(obj.user.first_name, obj.user.last_name)
+    user_full_name.short_description = "Post owner"
+    user_full_name.admin_order_field = "user__first_name"
