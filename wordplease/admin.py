b2011ccdbc0f2b24e8352a89c901c83b25a05b31
Luis Barriga <barbaclu@MacBook-Air-de-Luis.local>
2018-01-08 22:25:57 +0100

    Seguimos con la personalización del Admin de Django

--- wordplease/admin.py
+++ wordplease/admin.py
@@ -1,4 +1,5 @@
 from django.contrib import admin
+from django.utils.safestring import mark_safe
 
 from wordplease.models import Category, Post
 
@@ -10,7 +11,26 @@ class PostAdmin(admin.ModelAdmin):
     list_filter = ('user', 'category')
     search_fields = ('title', 'introduction')
 
-    def user_full_name(self, obj):
-        return "{0} {1}".format(obj.user.first_name, obj.user.last_name)
+    def user_full_name(self, post):
+        return "{0} {1}".format(post.user.first_name, post.user.last_name)
     user_full_name.short_description = "Post owner"
     user_full_name.admin_order_field = "user__first_name"
+
+    def get_image_html(self, post):
+        return mark_safe('<img src="{0}" alt="{1}" height="100">'.format(post.URL, post.title))
+    get_image_html.short_description = "Post image"
+
+    readonly_fields = ('get_image_html',)
+    fieldsets = (
+        (None, {
+            'fields':('title', 'introduction')
+        }),
+        ("Body, URL photo and date ", {
+            'fields': ('body', 'get_image_html', 'URL', 'date')
+        }),
+        ("User and Category", {
+            'fields': ('user', 'category'),
+            'classes':('collapse',),
+            'description': 'Foreign keys'
+        })
+    )
