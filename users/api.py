0c024510c47fa8e6b79a7dae4c1cdd7d268231a1
Luis Barriga <barbaclu@gmail.com>
2018-01-12 12:08:25 +0100

    Instalamos REST framework y cremo un endpoint Hello World

---
+++ users/api.py
@@ -0,0 +1,11 @@
+from rest_framework.response import Response
+from rest_framework.views import APIView
+
+
+class HelloWorld (APIView):
+
+    def get(self, request):
+        return Response({'hello': 'world'})
+
+    def post(self, request):
+        return Response(request.data)
--- wordPleaseBlogging/urls.py
+++ wordPleaseBlogging/urls.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
 from django.urls import path
+
+from users.api import HelloWorld
 from wordplease.views import home, post_detail, CreatePostView, MyPostView, PostsByUserName
 from users.views import LoginView, LogoutView, Blogs, SignUpView
 
@@ -11,13 +13,17 @@ urlpatterns = [
     path('blogs/<str:username>/<int:pk>', post_detail, name="post_detail_name"),
 
     path('blogs/', Blogs.as_view(), name="blogs_page"),
-    path('blogs/<str:username>/', PostsByUserName.as_view(), name='posts_by_name'),
+    path('blogs/<str:username>/', PostsByUserName.as_view(), name="posts_by_name"),
 
 
-    path('login', LoginView.as_view(), name='login_page'),
-    path('logout', LogoutView.as_view(), name='logout_page'),
-    path('signup/', SignUpView.as_view(), name='signup'),
+    path('login', LoginView.as_view(), name="login_page"),
+    path('logout', LogoutView.as_view(), name="logout_page"),
+    path('signup/', SignUpView.as_view(), name="signup"),
 
     path('new-post/', CreatePostView.as_view(), name="create_post_page"),
+
+    # API REST
+
+    path('api/1.0/hello/', HelloWorld.as_view(), name="api_hello_world")
 
 ]
