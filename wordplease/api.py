45c673c440ce3490da7fc8e95a5d0f6c4f5b3621
Luis Barriga <barbaclu@gmail.com>
2018-01-13 00:32:25 +0100

    Ponemos control de autenticación al endpoint de creación de usuarios y relacionamos el post con el usuario autenticado

--- wordplease/api.py
+++ wordplease/api.py
@@ -1,17 +1,26 @@
 from rest_framework.generics import ListCreateAPIView, RetrieveUpdateDestroyAPIView
-from rest_framework.response import Response
-from rest_framework.views import APIView
+from rest_framework.permissions import IsAuthenticatedOrReadOnly
 
 from wordplease.models import Post
-from wordplease.serializers import PostSerializer
+from wordplease.serializers import PostSerializer, PostsListSerializer
 
 
 class PostListAPI(ListCreateAPIView):
 
     queryset = Post.objects.all()
-    serializer_class = PostSerializer
+    permission_classes = [IsAuthenticatedOrReadOnly]
+
+    def get_serializer_class(self):
+        return PostsListSerializer if self.request.method == "GET" else PostSerializer
+
+    def perform_create(self, serializer):
+        serializer.save(user=self.request.user)
+
 
 class PostDetailAPI(RetrieveUpdateDestroyAPIView):
 
     queryset = Post.objects.all()
     serializer_class = PostSerializer
+
+    def perform_update(self, serializer):
+        serializer.save(user=self.request.user)
--- wordplease/serializers.py
+++ wordplease/serializers.py
@@ -1,9 +1,19 @@
 from rest_framework import serializers
 
+from users.serializers import UserSerializer
 from wordplease.models import Post
 
 
+class PostsListSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = Post
+        fields = ["title", "URL", "introduction", "date"]
+
+
 class PostSerializer(serializers.ModelSerializer):
+
+    user = UserSerializer(read_only=True)
 
     class Meta:
         model = Post
